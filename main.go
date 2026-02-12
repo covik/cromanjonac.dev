2c59f2fa8f607f2abe54b00a9a4a32d33540104d
Mate Nakić <mate.nakic@zth.dev>
2026-02-12 09:13:03 +0000

    serve (and protect)

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import "net/http"
+
 func main() {
-	println("Hello, World!")
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
